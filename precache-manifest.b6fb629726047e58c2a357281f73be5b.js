8a34da5e24e551bca40eb45ca18ad60aeb6b5d63
Kaija Dunklin <kaykay_s@yahoo.com>
2020-08-07 14:11:45 -0400

    Updates

---
+++ precache-manifest.b6fb629726047e58c2a357281f73be5b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "28b2cfe3fc4b7836c9c218e0e397d4a5",
+    "url": "/kaidunklin23/catFacts/index.html"
+  },
+  {
+    "revision": "a5083696054077b7656c",
+    "url": "/kaidunklin23/catFacts/static/css/main.92d60fbc.chunk.css"
+  },
+  {
+    "revision": "43a5021881fe423d4dd8",
+    "url": "/kaidunklin23/catFacts/static/js/2.f8e53260.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/kaidunklin23/catFacts/static/js/2.f8e53260.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a5083696054077b7656c",
+    "url": "/kaidunklin23/catFacts/static/js/main.db45ad85.chunk.js"
+  },
+  {
+    "revision": "1577899836333678a298",
+    "url": "/kaidunklin23/catFacts/static/js/runtime-main.d7a1d277.js"
+  },
+  {
+    "revision": "31c2b38470e75fa7c207363e9a4f2ee3",
+    "url": "/kaidunklin23/catFacts/static/media/giphy.31c2b384.gif"
+  }
+]);
